3ac471df7e7da0d356373cf84cb15fe501d1a2a5
Thor77 <thor77@thor77.org>
2018-12-02 01:03:03 +0100

    [01] Add solution in Python

---
+++ day01.py
@@ -0,0 +1,30 @@
+from time import time
+
+
+def part1(changes):
+    return sum(changes)
+
+
+def part2(changes):
+    frequencies = {0: None}
+    result = 0
+    while True:
+        for change in changes:
+            result += change
+            if result in frequencies:
+                return result
+            else:
+                frequencies[result] = None
+
+
+if __name__ == '__main__':
+    changes = []
+    with open('inputs/01') as f:
+        changes = list(map(int, filter(None, f.readlines())))
+    print(part1(changes))
+    times_sum = 0
+    for _ in range(101):
+        start = time()
+        part2(changes)
+        times_sum += time() - start
+    print(f'Mean over 100 runs: {(times_sum / 100) * 1000}ms')
