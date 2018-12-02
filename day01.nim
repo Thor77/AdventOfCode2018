aab031335454301cf46621bac424613c7d1ab13c
Thor77 <thor77@thor77.org>
2018-12-02 01:08:58 +0100

    [01] Use sets and add timing to nim solution

--- day01.nim
+++ day01.nim
@@ -1,19 +1,22 @@
 from utils import nil
 import strutils
 import sequtils
+import strformat
+import times
 import math
+import sets
 
 proc part1(changes: seq[int]): int =
     return changes.sum
 
 proc part2(changes: seq[int]): int =
-    var seen = @[0]
+    var seen = initSet[int]()
     while true:
         for change in changes:
             result += change
-            if result in seen:
+            if seen.contains(result):
                 return result
-            seen.add(result)
+            seen.incl(result)
 
 when isMainModule:
     var input = utils.readInputLines("01").map(parseInt)
@@ -22,4 +25,10 @@ when isMainModule:
     assert part2(@[+1, -1]) == 0
     assert part2(@[-6, +3, +8, +5, -6]) == 5
     assert part2(@[+7, +7, -2, -7, -4]) == 14
-    echo part2(input)
+    var sumTime: float
+    var startTime: float
+    for _ in 0..101:
+        startTime = epochTime()
+        discard part2(input)
+        sumTime += (epochTime() - startTime)
+    echo &"Mean over 100 runs: {(sumTime / 100) * 1000}ms"
